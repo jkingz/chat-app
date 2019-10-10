1d0826ca7e97ab12b5c8993b596f97452c08393f
jkingz <jfkrule@gmail.com>
2019-10-10 17:38:23 +0800

    Created user connection welcome messages

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const socketio = require('socket.io');
 const http = require('http');
 
 const router = require('./router');
+const { addUser, removeUser, getUser, getUserInRoom } = require('./users.js');
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,10 +12,18 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-    console.log('user connected');
-
     socket.on('join', ({ name, room }, callback) => {
-        console.log(name, room, "backend data");
+        const { error, user } = addUser({ id: socket.id, name, room });
+
+        if(error) return callback(error);
+
+        socket.emit('message', { user: 'admin', text: `${user.name}, Welcome to the room ${user.room}`});
+
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined!`});
+
+        socket.join(user.room);
+
+        callback();
     });
 
     socket.on('disconnect', () => {
